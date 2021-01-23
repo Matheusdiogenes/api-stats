5f8a92aa778871d8024b75d122c15cd4c3398cf1
Matheus Diogenes <matheusdiogenes98@gmail.com>
2021-01-22 21:00:00 -0300

    Configurando database

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-const routerTeamGoal = require('./src/routes/teamGoal')
+const routerGoal = require('./src/routes/teamGoal')
 
-app.use(express.json())
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
-app.use('/goal', routerTeamGoal)
+app.use('/goal', routerGoal)
 
 app.listen(port, () => {
   console.log('Conectado...');
