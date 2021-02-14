13a6654fa59c90d38dbd8f6f9359389b8a25696d
Matheus Diogenes <matheusdiogenes98@gmail.com>
2021-02-13 21:05:53 -0300

    Novas rotas e controllers (Goal)

--- src/controllers/goal/halfFull.js
+++ src/controllers/goal/halfFull.js
@@ -1,5 +1,6 @@
 const pg = require('../../database/pg')
 module.exports = {
+  // FULL  
   fullTime: (req, res) => {
     const query = {
       text: `WITH geral as (
@@ -42,6 +43,7 @@ module.exports = {
         FROM "seriea_ita" WHERE lower(team_away) = $1
       ) 
         SELECT
+          "team",
           count('team') as "game",
           count(case when "for" > "concede" then 1 end) as "win",
           count(case when "for" = "concede" then 1 end) as "draw",
@@ -51,7 +53,82 @@ module.exports = {
           round(avg("for"+ "concede"), 2) as "avg_total"
         FROM geral
         WHERE game_date in (SELECT game_date FROM geral order by game_date desc LIMIT $2 )
-      `,
+        GROUP BY "team"`,
+      values: [team, game]
+    } 
+    pg.pool.query(query, (err, result) => {
+      if(err) {
+        throw err
+      }     
+      res.status(200).json(result.rows)
+    })  
+  },  
+
+  fullTimeHome: (req, res) => {
+    const query = {
+      text: `SELECT team_home,
+              count(team_home) as "game",
+              count(case when home_ft > away_ft then 1 end) as "win",
+              count(case when home_ft = away_ft then 1 end) as "draw",
+              count(case when home_ft < away_ft then 1 end) as "loss",
+              round(avg(home_ft), 2) as "avg_for",
+              round(avg(away_ft), 2) as "avg_concede",
+              round(avg(home_ft+ away_ft), 2) as "avg_total"
+            FROM "seriea_ita"
+            GROUP BY team_home order by team_home
+       `
+    } 
+    pg.pool.query(query, (err, result) => {
+      if(err) {
+        throw err
+      }     
+      res.status(200).json(result.rows)
+    })  
+  },
+
+  fullTimeAway: (req, res) => {
+    const query = {
+      text: `SELECT team_away,
+              count(team_away) as "game",
+              count(case when away_ft > home_ft then 1 end) as "win",
+              count(case when away_ft = home_ft then 1 end) as "draw",
+              count(case when away_ft < home_ft then 1 end) as "loss",
+              round(avg(home_ft), 2) as "avg_for",
+              round(avg(away_ft), 2) as "avg_concede",
+              round(avg(home_ft+ away_ft), 2) as "avg_total"
+            FROM "seriea_ita"
+            GROUP BY team_away order by team_away
+       `
+    } 
+    pg.pool.query(query, (err, result) => {
+      if(err) {
+        throw err
+      }     
+      res.status(200).json(result.rows)
+    })  
+  },
+
+  latestFullHome: (req, res) => {
+    const team = req.params['team'].replace('-', ' ').toLowerCase()
+    const game = req.params['latest']
+    const query = {
+      text: `
+      WITH geral as (
+        SELECT game_date, team_home, home_ft, away_ft
+          FROM "seriea_ita" WHERE lower(team_home) = $1        
+      ) 
+        SELECT
+          team_home,
+          count(team_home) as "game",
+          count(case when home_ft > away_ft then 1 end) as "win",
+          count(case when home_ft = away_ft then 1 end) as "draw",
+          count(case when home_ft < away_ft then 1 end) as "loss",
+          round(avg(home_ft ), 2) as "avg_for",
+          round(avg(away_ft), 2) as "avg_concede",
+          round(avg(home_ft + away_ft), 2) as "avg_total"
+        FROM geral
+        WHERE game_date in (SELECT game_date FROM geral order by game_date desc LIMIT $2 )
+        GROUP BY team_home`,
       values: [team, game]
     } 
     pg.pool.query(query, (err, result) => {
@@ -61,6 +138,38 @@ module.exports = {
       res.status(200).json(result.rows)
     })  
   },
+
+  latestFullAway: (req, res) => {
+    const team = req.params['team'].replace('-', ' ').toLowerCase()
+    const game = req.params['latest']
+    const query = {
+      text: `
+      WITH geral as (
+        SELECT game_date, team_home, home_ft, away_ft
+          FROM "seriea_ita" WHERE lower(team_home) = $1        
+      ) 
+        SELECT
+          team_home,
+          count(team_home) as "game",
+          count(case when away_ft > home_ft then 1 end) as "win",
+          count(case when away_ft = home_ft then 1 end) as "draw",
+          count(case when away_ft < home_ft then 1 end) as "loss",
+          round(avg(away_ft), 2) as "avg_for",
+          round(avg(home_ft), 2) as "avg_concede",
+          round(avg(home_ft + away_ft), 2) as "avg_total"
+        FROM geral
+        WHERE game_date in (SELECT game_date FROM geral order by game_date desc LIMIT $2 )
+        GROUP BY team_home`,
+      values: [team, game]
+    } 
+    pg.pool.query(query, (err, result) => {
+      if(err) {
+        throw err
+      }     
+      res.status(200).json(result.rows)
+    })  
+  },
+  //HALF
 
   halftime: (req, res) => {
     const query = {
@@ -105,6 +214,7 @@ module.exports = {
         FROM "seriea_ita" WHERE lower(team_away) = $1
       ) 
         SELECT
+          "team",
           count('team') as "game",
           count(case when "for" > "concede" then 1 end) as "win",
           count(case when "for" = "concede" then 1 end) as "draw",
@@ -114,6 +224,7 @@ module.exports = {
           round(avg("for"+ "concede"), 2) as "avg_total"
         FROM geral
         WHERE game_date in (SELECT game_date FROM geral order by game_date desc LIMIT $2 )
+        GROUP BY "team"
       `,
       values: [team, game]
     } 
@@ -123,8 +234,113 @@ module.exports = {
       }     
       res.status(200).json(result.rows)
     })  
-  }
+  },
 
+  homeHT: (req, res) => {
+    const query = {
+      text: `SELECT team_home,
+              count(team_home) as "game",
+              count(case when home_ht > away_ht then 1 end) as "win",
+              count(case when home_ht = away_ht then 1 end) as "draw",
+              count(case when home_ht < away_ht then 1 end) as "loss",
+              round(avg(home_ht), 2) as "avg_for",
+              round(avg(away_ht), 2) as "avg_concede",
+              round(avg(home_ht+ away_ht), 2) as "avg_total"
+            FROM "seriea_ita"
+            GROUP BY team_home order by team_home
+       `
+    } 
+    pg.pool.query(query, (err, result) => {
+      if(err) {
+        throw err
+      }     
+      res.status(200).json(result.rows)
+    })  
+  },
+
+  latestHomeHT: (req, res) => {
+    const team = req.params['team'].replace('-', ' ').toLowerCase()
+    const game = req.params['latest']
+    const query = {
+      text: `
+      WITH geral as (
+        SELECT game_date, team_home, home_ht, away_ht
+          FROM "seriea_ita" WHERE lower(team_home) = $1        
+      ) 
+        SELECT
+          team_home,
+          count(team_home) as "game",
+          count(case when home_ht > away_ht then 1 end) as "win",
+          count(case when home_ht = away_ht then 1 end) as "draw",
+          count(case when home_ht < away_ht then 1 end) as "loss",
+          round(avg(home_ht ), 2) as "avg_for",
+          round(avg(away_ht), 2) as "avg_concede",
+          round(avg(home_ht + away_ht), 2) as "avg_total"
+        FROM geral
+        WHERE game_date in (SELECT game_date FROM geral order by game_date desc LIMIT $2 )
+        GROUP BY team_home`,
+      values: [team, game]
+    } 
+    pg.pool.query(query, (err, result) => {
+      if(err) {
+        throw err
+      }     
+      res.status(200).json(result.rows)
+    })  
+  },
+
+  awayHT: (req, res) => {
+    const query = {
+      text: `SELECT team_away,
+              count(team_away) as "game",
+              count(case when away_ht > home_ht then 1 end) as "win",
+              count(case when away_ht = home_ht then 1 end) as "draw",
+              count(case when away_ht < home_ht then 1 end) as "loss",
+              round(avg(away_ht), 2) as "avg_for",
+              round(avg(home_ht), 2) as "avg_concede",
+              round(avg(home_ht + away_ht), 2) as "avg_total"
+            FROM "seriea_ita"
+            GROUP BY team_away order by team_away
+       `
+    } 
+    pg.pool.query(query, (err, result) => {
+      if(err) {
+        throw err
+      }     
+      res.status(200).json(result.rows)
+    })  
+  },
+
+  latestAwayHT: (req, res) => {
+    const team = req.params['team'].replace('-', ' ').toLowerCase()
+    const game = req.params['latest']
+    const query = {
+      text: `
+      WITH geral as (
+        SELECT game_date, team_away, home_ht, away_ht
+          FROM "seriea_ita" WHERE lower(team_away) = $1        
+      ) 
+        SELECT
+          team_away,
+          count(team_away) as "game",
+          count(case when away_ht > home_ht then 1 end) as "win",
+          count(case when away_ht = home_ht then 1 end) as "draw",
+          count(case when away_ht < home_ht then 1 end) as "loss",
+          round(avg(away_ht ), 2) as "avg_for",
+          round(avg(home_ht), 2) as "avg_concede",
+          round(avg(home_ht + away_ht), 2) as "avg_total"
+        FROM geral
+        WHERE game_date in (SELECT game_date FROM geral order by game_date desc LIMIT $2 )
+        GROUP BY team_away`,
+      values: [team, game]
+    } 
+    pg.pool.query(query, (err, result) => {
+      if(err) {
+        throw err
+      }     
+      res.status(200).json(result.rows)
+    })  
+  }
 
  
 }
---
+++ src/controllers/goal/info.js
@@ -0,0 +1,105 @@
+const pg = require('../../database/pg')
+module.exports = {
+
+  info: (req, res) => {
+    const team = req.params['team'].replace('-', ' ').toLowerCase()
+    const game = req.params['latest']
+    const query = {
+      text: `
+      WITH geral as (
+        SELECT game_date, team_home as "team", home_ht as "forHT", away_ht as "concedeHT", home_ft as "for", away_ft as "concede"
+        FROM "seriea_ita" WHERE lower(team_home) = $1
+        union all
+        SELECT game_date, team_away, away_ht, home_ht, away_ft, home_ft
+        FROM "seriea_ita" WHERE lower(team_away) = $1
+      )         
+        SELECT "team",     
+          count("team") as "game",
+          count(case when "for" > "concede" then 1 end) as "win",
+          count(case when "for" = "concede" then 1 end) as "draw",
+          count(case when "for" < "concede" then 1 end) as "loss",
+          round(avg("for"), 2) as "for", 
+          round(avg("concede"), 2) as "concede", 
+          round(avg("forHT"), 2) as "for_ht", 
+          round(avg("concedeHT"), 2) as "concede_ht",
+          count(case when ("forHT" + "concedeHT") > 0 then 1 end) as "Over 0,5 ht",
+          count(case when ("for" + "concede") > 1 then 1 end) as "Over 1.5",
+          count(case when ("for" + "concede") > 2 then 1 end) as "Over 2.5",
+          count(case when ("for" + "concede") < 2 then 1 end) as "Under 1.5",
+          count(case when ("for" + "concede") < 3 then 1 end) as "Under 2.5",
+          count(case when "for" > 0 and "concede" > 0 then 1 end) as "Btts"
+        FROM geral
+        WHERE game_date in (SELECT game_date FROM geral order by game_date desc LIMIT $2 )
+        GROUP BY "team"`,
+      values: [team, game]
+    } 
+    pg.pool.query(query, (err, result) => {
+      if(err) {
+        throw err
+      }     
+      res.status(200).json(result.rows)
+    })  
+  },
+
+  compare: (req, res) => {
+    const teamHome = req.params['teamHome'].replace('-', ' ').toLowerCase()
+    const teamAway = req.params['teamAway'].replace('-', ' ').toLowerCase()
+    const game = req.params['latest']
+    const query = {
+      text: `
+      WITH geral as (
+        SELECT game_date, team_home as "team", home_ft as "for", away_ft as "concede", home_ht as "forHT", away_ht as "concedeHT"        
+        FROM "seriea_ita" WHERE lower(team_home) = $1
+        union all
+        SELECT game_date, team_away, away_ft, home_ft, away_ht, home_ht         
+        FROM "seriea_ita" WHERE lower(team_away) = $2
+      ) 
+        SELECT "team",     
+          count("team") as "game",
+          count(case when "for" > "concede" then 1 end) as "win",
+          count(case when "for" = "concede" then 1 end) as "draw",
+          count(case when "for" < "concede" then 1 end) as "loss",
+          round(avg("for"), 2) as "for", 
+          round(avg("concede"), 2) as "concede", 
+          round(avg("forHT"), 2) as "for_ht", 
+          round(avg("concedeHT"), 2) as "concede_ht",
+          count(case when ("forHT" + "concedeHT") > 0 then 1 end) as "Over 0,5 ht",
+          count(case when ("for" + "concede") > 1 then 1 end) as "Over 1.5",
+          count(case when ("for" + "concede") > 2 then 1 end) as "Over 2.5",
+          count(case when ("for" + "concede") < 2 then 1 end) as "Under 1.5",
+          count(case when ("for" + "concede") < 3 then 1 end) as "Under 2.5",
+          count(case when "for" > 0 and "concede" > 0 then 1 end) as "Btts"
+        FROM geral 
+        WHERE lower("team") = $1 and game_date in (SELECT game_date FROM geral where lower("team") = $1 order by game_date desc LIMIT $3 ) 
+        GROUP BY "team"
+        union
+        SELECT "team",     
+          count("team") as "game",
+          count(case when "for" > "concede" then 1 end) as "win",
+          count(case when "for" = "concede" then 1 end) as "draw",
+          count(case when "for" < "concede" then 1 end) as "loss",
+          round(avg("for"), 2) as "for", 
+          round(avg("concede"), 2) as "concede", 
+          round(avg("forHT"), 2) as "for_ht", 
+          round(avg("concedeHT"), 2) as "concede_ht",
+          count(case when ("forHT" + "concedeHT") > 0 then 1 end) as "Over 0,5 ht",
+          count(case when ("for" + "concede") > 1 then 1 end) as "Over 1.5",
+          count(case when ("for" + "concede") > 2 then 1 end) as "Over 2.5",
+          count(case when ("for" + "concede") < 2 then 1 end) as "Under 1.5",
+          count(case when ("for" + "concede") < 3 then 1 end) as "Under 2.5",
+          count(case when "for" > 0 and "concede" > 0 then 1 end) as "Btts"
+        FROM geral 
+        WHERE lower("team") = $2 and game_date in (SELECT game_date FROM geral where lower("team") = $2 order by game_date desc LIMIT $3 ) 
+        GROUP BY "team"`,
+      values: [teamHome, teamAway, game]
+    } 
+    pg.pool.query(query, (err, result) => {
+      if(err) {
+        throw err
+      }     
+      res.status(200).json(result.rows)
+    })  
+  }
+
+
+}
